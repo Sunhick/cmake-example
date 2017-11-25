fe2d08da40e56ed62153c25c92db19af19acc056
Sunil <sunhick@gmail.com>
2017-11-25 13:02:25 -0800

    Added configure file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <config.h>
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   cout << "Welcome to cmake tutorials!" << "\n";
+
+  cout << "Author: " << AUTHOR << "\n";
   return 0;
 }
