e38bb15f8673d4bbd96995551fa66d03abd3c808
Sunil <sunhick@gmail.com>
2017-12-10 21:15:57 -0800

    add flymake for dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
-((nil . ((eval . (linum-mode 1)))))
+((nil . ((eval . (linum-mode 1))
+         (eval . (flycheck-mode 1)))))
